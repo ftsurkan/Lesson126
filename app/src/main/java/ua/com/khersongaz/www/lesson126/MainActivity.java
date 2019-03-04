33af3f330823474a9493afcf28655eac29398ac9
ftsurkan <wherepone@gmail.com>
2019-03-04 16:58:54 +0200

    working now

--- app/src/main/java/ua/com/khersongaz/www/lesson126/MainActivity.java
+++ app/src/main/java/ua/com/khersongaz/www/lesson126/MainActivity.java
@@ -1,13 +1,233 @@
 package ua.com.khersongaz.www.lesson126;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.ContentUris;
+import android.content.pm.PackageManager;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+
+
+import android.media.MediaPlayer.OnCompletionListener;
+import android.media.MediaPlayer.OnPreparedListener;
+
+
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity
+        implements OnPreparedListener,
+        OnCompletionListener {
+
+
+    final String LOG_TAG = "myLogs";
+
+    final String DATA_HTTP = "http://10.0.2.2/mpthreetest.mp3";
+    final String DATA_STREAM = "http://online.radiorecord.ru:8101/rr_128";
+    final String DATA_SD = "/sdcard/Music/file2.mp3" ;
+            //Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC)
+            //+ "/file2.mp3";
+
+    /* /sdcard/Music/file_example_MP3_700KB.mp3 */
+
+    final Uri DATA_URI = ContentUris
+            .withAppendedId(
+                    android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                    13359);
+
+    MediaPlayer mediaPlayer;
+    AudioManager am;
+    CheckBox chbLoop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        am = (AudioManager) getSystemService(AUDIO_SERVICE);
+        chbLoop = (CheckBox) findViewById(R.id.chbLoop);
+
+        chbLoop.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,
+                                         boolean isChecked) {
+                if (mediaPlayer != null)
+                    mediaPlayer.setLooping(isChecked);
+            }
+        });
+    }
+
+
+
+
+    // 2019-03-04 16:37
+    // Storage Permissions
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    /**
+     * Checks if the app has permission to write to device storage
+     *
+     * If the app does not has permission then the user will be prompted to grant permissions
+     *
+     * @param activity
+     */
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult (int requestCode,
+                                     String[] permissions,
+                                     int[] grantResults)
+    {
+
+    }
+
+
+
+    public void onClickStart(View view) {
+        releaseMP();
+
+        try {
+            switch (view.getId()) {
+                case R.id.btnStartHttp:
+                    Log.d(LOG_TAG, "start HTTP");
+                    mediaPlayer = new MediaPlayer();
+                    mediaPlayer.setDataSource(DATA_HTTP);
+                    mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                    Log.d(LOG_TAG, "prepareAsync");
+                    mediaPlayer.setOnPreparedListener(this);
+                    mediaPlayer.prepareAsync();
+                    break;
+                case R.id.btnStartStream:
+                    Log.d(LOG_TAG, "start Stream");
+                    mediaPlayer = new MediaPlayer();
+                    mediaPlayer.setDataSource(DATA_STREAM);
+                    mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                    Log.d(LOG_TAG, "prepareAsync");
+                    mediaPlayer.setOnPreparedListener(this);
+                    mediaPlayer.prepareAsync();
+                    break;
+                case R.id.btnStartSD:
+
+                    verifyStoragePermissions(this);
+
+                    Log.d(LOG_TAG, "start SD");
+                    mediaPlayer = new MediaPlayer();
+                    mediaPlayer.setDataSource(DATA_SD);
+                    mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                    mediaPlayer.prepare();
+                    mediaPlayer.start();
+                    break;
+                case R.id.btnStartUri:
+                    Log.d(LOG_TAG, "start Uri");
+                    mediaPlayer = new MediaPlayer();
+                    mediaPlayer.setDataSource(this, DATA_URI);
+                    mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                    mediaPlayer.prepare();
+                    mediaPlayer.start();
+                    break;
+                case R.id.btnStartRaw:
+                    Log.d(LOG_TAG, "start Raw");
+                    mediaPlayer = MediaPlayer.create(this, R.raw.file_example);
+                    mediaPlayer.start();
+                    break;
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (mediaPlayer == null)
+            return;
+
+        mediaPlayer.setLooping(chbLoop.isChecked());
+        mediaPlayer.setOnCompletionListener(this);
+    }
+
+    private void releaseMP() {
+        if (mediaPlayer != null) {
+            try {
+                mediaPlayer.release();
+                mediaPlayer = null;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void onClick(View view) {
+        if (mediaPlayer == null)
+            return;
+        switch (view.getId()) {
+            case R.id.btnPause:
+                if (mediaPlayer.isPlaying())
+                    mediaPlayer.pause();
+                break;
+            case R.id.btnResume:
+                if (!mediaPlayer.isPlaying())
+                    mediaPlayer.start();
+                break;
+            case R.id.btnStop:
+                mediaPlayer.stop();
+                break;
+            case R.id.btnBackward:
+                mediaPlayer.seekTo(mediaPlayer.getCurrentPosition() - 3000);
+                break;
+            case R.id.btnForward:
+                mediaPlayer.seekTo(mediaPlayer.getCurrentPosition() + 3000);
+                break;
+            case R.id.btnInfo:
+                Log.d(LOG_TAG, "Playing " + mediaPlayer.isPlaying());
+                Log.d(LOG_TAG, "Time " + mediaPlayer.getCurrentPosition() + " / "
+                        + mediaPlayer.getDuration());
+                Log.d(LOG_TAG, "Looping " + mediaPlayer.isLooping());
+                Log.d(LOG_TAG,
+                        "Volume " + am.getStreamVolume(AudioManager.STREAM_MUSIC));
+                break;
+
+        }
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        Log.d(LOG_TAG, "onPrepared");
+        mp.start();
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        Log.d(LOG_TAG, "onCompletion");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        releaseMP();
     }
 }
